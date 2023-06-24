03d13a56db476f31d0f13ee574a1f27ccd4458a7
krastarasta <kristian@hearttec.no>
2023-06-24 08:39:04 +0200

    Building the swipe component.

---
+++ swiper.js
@@ -0,0 +1,15 @@
+
+  <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+    var swiper = new Swiper(".mySwiper", {
+      cssMode: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+    });
